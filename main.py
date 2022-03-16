27281c7a1cf5b7656e6625e18650e5036f22c7e1
Kamil Salawa <emir.salawa@gmail.com>
2022-03-16 20:32:51 +0000

    added try error functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
@@ -33,21 +34,34 @@ def save():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
+    new_data = {website: {
+        "email": email,
+        "password": password,
+        }
+    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ooooopsies", message="You've left some fields empty, please dinnae!")
 
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered: \nEmail: {email}\nPassword: {password}\n"
-                                               f"Is it ok to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                #reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #updating old data
+            data.update(new_data)
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password} \n")
+            with open("data.json", "w") as data_file:
+                #saving updated data
+                json.dump(data, data_file, indent=4)
 
-                website_input.delete(0, END)
-                password_input.delete(0, END)
+        finally:
+            website_input.delete(0, END)
+            password_input.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
